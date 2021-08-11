4d08f2d31e37115a42ee6c8019efc30a752d15e9
Liang Ding <d@b3log.org>
2021-08-11 14:27:58 +0800

    :memo: Fix typo

--- aes.go
+++ aes.go
@@ -1,7 +1,7 @@
 // Encryption - AES-GCM 端到端加密
 // Copyright (c) 2021-present, b3log.org
 //
-// Lute is licensed under Mulan PSL v2.
+// Encryption is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
 //         http://license.coscl.org.cn/MulanPSL2
